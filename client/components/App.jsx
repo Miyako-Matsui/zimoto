2fd0da22e8459182ea63351b60cdd86187ab4c59
Miyako Matsui <miyakomatsui18@gmail.com>
2022-12-01 20:52:07 +1300

    add Profile component + add profile route on App

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -2,6 +2,7 @@ import React, { useEffect } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { Route, Routes } from 'react-router-dom'
 import Home from './Home'
+import Profile from './Profile'
 
 function App() {
   // const fruits = useSelector((state) => state.fruits)
@@ -13,7 +14,8 @@ function App() {
   return (
     <>
       <Routes>
-        <Route path='/' element={<Home />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/profile" element={<Profile />} />
       </Routes>
     </>
   )
