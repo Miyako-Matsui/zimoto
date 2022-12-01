48602ede952745752170752b9e7f9ec9a2f46844
Eben Serfontein <ebenserf@gmail.com>
2022-12-02 11:24:13 +1300

    setting up getAGuide api WIP

--- client/components/GuideProfile.jsx
+++ client/components/GuideProfile.jsx
@@ -1,30 +1,40 @@
-import React from 'react'
-
+import React, { useEffect } from 'react'
+import { fetchAGuide } from '../apis/individualGuide'
+import '../index.css'
 
 function GuideProfile() {
-    
-    return (
+  useEffect(() => {
+    fetchAGuide(3)
+      .then((res) => {
+        console.log(res)
+      })
+      .catch((err) => {
+        console.log(err.message)
+      })
+  }, [])
+
+  return (
     <>
-    <div>
-      <h1>This is a profile!</h1>
-      <h2>{name}</h2>
-      <h2>Country</h2>
-      <p>{country}</p>
-      <h2>City</h2>
-      <p>{city}</p>
-      <h2>Bio</h2>
-      <p>{bio}</p>
-      <h2>Spoken languages</h2>
-      <p>{language}</p>
-      <h2>Fee</h2>
-      <p>{fee}</p>
-      <h2>Contact Number</h2>
-      <p>{contact_number}</p>
-      <h2>Email</h2>
-      <p>{email}</p>
-    </div>
-        
+      <div>
+        {/* <h1>This is a profile!</h1>
+        <h2>{name}</h2>
+        <h2>Country</h2>
+        <p>{country}</p>
+        <h2>City</h2>
+        <p>{city}</p>
+        <h2>Bio</h2>
+        <p>{bio}</p>
+        <h2>Spoken languages</h2>
+        <p>{language}</p>
+        <h2>Fee</h2>
+        <p>{fee}</p>
+        <h2>Contact Number</h2>
+        <p>{contact_number}</p>
+        <h2>Email</h2>
+        <p>{email}</p> */}
+      </div>
     </>
-)}
+  )
+}
 
 export default GuideProfile
