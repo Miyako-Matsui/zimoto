b106f38295a83e14801f748fb48f1496b63ff095
ashley cao <112398477+ashley-cao-0@users.noreply.github.com>
2022-12-02 11:54:14 +1300

    clean up

--- server/routes/guides.js
+++ server/routes/guides.js
@@ -4,14 +4,14 @@ const db = require('../db/db.js')
 
 const router = express.Router()
 
+//get all guides
 router.get('/', (req, res) => {
   db.getGuides()
     .then((results) => {
       res.json(results)
     })
     .catch((err) => {
-      console.log(err)
-      res.status(500).json({ message: 'Something went wrong' })
+      res.status(500).json({ message: err.message})
     })
 })
 
