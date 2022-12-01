e2adca104c45e5fa6727aabbb7cf82f0e4278f68
melissa-forbes <34205898+melforbes@users.noreply.github.com>
2022-12-02 10:31:51 +1300

    set up routes, db connection and function

--- server/routes/profilesRouter.js
+++ server/routes/profilesRouter.js
@@ -3,23 +3,12 @@ const router = express.Router()
 
 const db = require('../db/db')
 
-router.get('/', (req, res) => {
-    db.getAllMovies()
-      .then((moviesArr) => {res.json(moviesArr)})
-      .catch((err) => {res.status(500).json({ message: err.message})})
-})
+router.get('/:id', (req, res) => {
+    const id = req.params.id
 
-router.post('/', (req, res) => {
-  const movie = req.body
-  db.addAMovie(movie)
-    .then((idArr) => {
-      const id = idArr[0]
-      return db.getMovieById(id)
-    })
-    .then((movieFromDb) => {
-      res.json(movieFromDb)
-    })
-    .catch((err) => {res.status(500).json({ message: err.message})})
+    db.getAGuide(id)
+      .then((guideArr) => {console.log(guideArr),res.json(guideArr)})
+      .catch((err) => {res.status(500).json({ message: err.message})})
 })
 
 module.exports = router
