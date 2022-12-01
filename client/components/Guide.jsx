a21ee70348cdacff4ddfa5ad5c84fd0bcf1015af
ashley cao <112398477+ashley-cao-0@users.noreply.github.com>
2022-12-02 11:59:58 +1300

    add link to profile

--- client/components/Guide.jsx
+++ client/components/Guide.jsx
@@ -1,13 +1,16 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import '../index.css'
 
 function Guide({guide}) {
-  const {name, averageRating} = guide
+  const {id, name, averageRating} = guide
   return(
-    <div className=' rounded shadow-md p-2 m-2'>
-      <h1 className=" text-xl"> {name} </h1>
-      <h2> Rating: {averageRating} </h2>
-    </div>
+    <Link to= {'/profiles/'+ id } >
+      <div className=' rounded shadow-md p-2 m-2'>
+        <h1 className=" text-xl"> {name} </h1>
+        <h2> Rating: {averageRating} </h2>
+      </div>
+    </Link>
   )
 }
 
