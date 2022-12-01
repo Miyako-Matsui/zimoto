fa9cd7e82576b53e4172d939884216b893a62d41
Eben Serfontein <ebenserf@gmail.com>
2022-12-02 10:40:41 +1300

    getProfile by id

--- server/db/db.js
+++ server/db/db.js
@@ -3,15 +3,15 @@ const connection = require('./connection')
 function getGuides(db = connection) {
   return db('guides').select()
 }
-function getAGuide (id, db = connection) {
+function getAGuide(id, db = connection) {
   return db('guides')
-  .join('guides', 'locations.guide_id', 'guides.id')
-  .select('*', 'guide_id AS guideId')
-  .where('guides.id', id)
-  .first()
+    .join('locations', 'locations.guide_id', 'guides.id')
+    .select('*', 'guide_id AS guideId')
+    .where('guides.id', id)
+    .first()
 }
 
 module.exports = {
   getGuides,
-  getAGuide
+  getAGuide,
 }
--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ const path = require('path')
 
 const guideRoutes = require('./routes/guides')
 const reviewsRoutes = require('./routes/reviews')
+const profileRoutes = require('./routes/profiles')
 
 const server = express()
 
@@ -10,6 +11,8 @@ server.use(express.json())
 server.use(express.static(path.join(__dirname, 'public')))
 
 server.use('/api/v1/guides', guideRoutes)
+
+server.use('/api/v1/profiles', profileRoutes)
 
 server.use('/api/v1/reviews', reviewsRoutes)
 
