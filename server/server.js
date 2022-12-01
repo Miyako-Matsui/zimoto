2be73f1f3040bcb9a288f27c2d53841ce6414711
ashley cao <112398477+ashley-cao-0@users.noreply.github.com>
2022-12-02 08:15:36 +1300

    delete duplicate line

--- server/server.js
+++ server/server.js
@@ -9,7 +9,7 @@ const server = express()
 server.use(express.json())
 server.use(express.static(path.join(__dirname, 'public')))
 
-server.use('/api/v1/reviews', reviewRoutes)
+
 server.get('*', (req, res) => {
   res.sendFile(path.resolve('server/public/index.html'))
 })
