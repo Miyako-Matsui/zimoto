61285411a64afda0ca3bd29a2534a2334c38e027
ashley cao <112398477+ashley-cao-0@users.noreply.github.com>
2022-12-02 15:39:12 +1300

    add review component

--- client/components/Review.jsx
+++ client/components/Review.jsx
@@ -1,53 +1,18 @@
-// import React, { useEffect } from 'react'
-// // import '../index.css'
-// import { useSelector, useDispatch } from 'react-redux'
-// import { fetchReviewsThunk } from '../actions'
-
-// export default function Review() {
-//   const dispatch = useDispatch()
-
-//   const reviews = useSelector(
-//     (globalState) => globalState.reviewsReducer.reviews
-//   )
-
-//   useEffect(() => {
-//     dispatch(fetchReviewsThunk())
-//   }, [])
-
-//   return (
-//     <>
-//       <div>
-//         {reviews &&
-//           reviews.map((review) => {
-//             return (
-//               <>
-//                 <div>
-//                   <p>{review.rating}</p>
-//                   <p>{review.title}</p>
-//                   <p>{review.text}</p>
-//                 </div>
-//               </>
-//             )
-//           })}
-//       </div>
-//     </>
-//   )
-// }
-
 import React from 'react'
+import { useSelector } from 'react-redux'
 import { Link } from 'react-router-dom'
-// import '../index.css'
 
-function Review({ review }) {
-  const { rating, title, text, guide_id } = review
+function Review({guideId}) {
+  const reviews = useSelector(state => state.reviews)
   return (
-    <Link to={'/reviews/reviews' + guide_id}>
       <div>
-        <h1> {rating} </h1>
-        <h1> {title} </h1>
-        <h1> {text} </h1>
+        {reviews.map(review => 
+          <div>
+            <h1> {review.title} </h1>
+            <p> {review.text} </p>
+          </div>
+          )}
       </div>
-    </Link>
   )
 }
 
