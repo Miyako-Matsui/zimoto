d432d0c388fbcfae00255b60a58c8cf2e51b47bb
Eben Serfontein <ebenserf@gmail.com>
2022-12-03 15:19:57 +1300

    WIP on redirecting upon db update

--- client/components/AddProfile.jsx
+++ client/components/AddProfile.jsx
@@ -1,10 +1,12 @@
 import React, { useEffect, useState } from 'react'
 import { addGuide } from '.././apis/guides'
 import { fetchAGuide } from '../apis/individualGuide'
+import { useNavigate } from 'react-router-dom'
 
 function addProfile() {
+  const navigate = useNavigate()
+
   const newGuide = {
-    id: 13,
     name: 'Buster Brown',
     bio: "He's the man",
     language: 'Space-Elvish, Romulyn',
@@ -13,19 +15,21 @@ function addProfile() {
     email: 'B.B@gmail.com',
   }
 
-  const [profile, setProfile] = useState([newGuide])
-
   useEffect(() => {
     addGuide(newGuide)
       .then((res) => {
-        setProfile(fetchAGuide(res[0]))
+        console.log(res)
       })
       .catch((err) => {
         console.log(err.message)
       })
   }, [])
 
-  return <h2>{profile?.name}</h2>
+  const testButton = () => {
+    return navigate('/')
+  }
+
+  return <button onClick={testButton}>Go home</button>
 }
 
 export default addProfile
