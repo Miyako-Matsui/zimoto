a611d77f84121d1519f79bd60ca3d7e6630cd02b
ashley cao <112398477+ashley-cao-0@users.noreply.github.com>
2022-12-03 15:47:45 +1300

    change join to leftJoin, add new db functions wIP

--- server/db/db.js
+++ server/db/db.js
@@ -1,15 +1,32 @@
+const { default: knex } = require('knex')
 const connection = require('./connection')
 
+function guideJoinTable(db = connection){
+  return db('guides')
+    .select('*', 'guides.id AS guide_id', 'contact_number AS contactNumber')
+    .leftJoin('reviews', 'guides.id', 'reviews.guide_id')
+    .leftJoin('locations', 'locations.guide_id', 'guides.id')
+    .avg('rating AS averageRating')
+    .groupBy('guides.id')
+}
+
 function getGuides(db = connection) {
-  // return db('guides')
-  //   .select('*', 'contact_number AS contactNumber')
-  return db('reviews')
-    .join('guides', 'guides.id', 'reviews.guide_id')
+  return db('guides')
+    .leftJoin('reviews', 'guides.id', 'reviews.guide_id')
     .select('*', 'guides.id AS guide_id', 'contact_number AS contactNumber')
     .avg('rating AS averageRating')
     .groupBy('guide_id')
 }
 
+function getFilteredGuides(filters, db = connection) {
+  return guideJoinTable()
+  // return db.select()
+  //   .from(getGuides())
+  //   .join('locations', 'locations.guide_id', 'guides.id')
+  //   .where('averageRating', '>', 3)
+}
+
 module.exports = {
   getGuides,
+  getFilteredGuides
 }
