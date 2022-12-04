aabdfded6dc190b02261ca992b129f6dad0c5d33
Miyako Matsui <miyakomatsui18@gmail.com>
2022-12-05 12:19:25 +1300

    wip css wip

--- client/components/GuideProfile.jsx
+++ client/components/GuideProfile.jsx
@@ -24,30 +24,45 @@ function GuideProfile() {
   return (
     <div className="w-1/2 mx-auto text-center flex flex-col">
       <div className="font-[Lora] bg-[#C2DEDC] text-xl ">
-        <div className="border-2[#C2DEDC] block pt-16">
+        <div className="border-2[#C2DEDC] block pt-16 break-all ">
           <div className=" flex justify-center space-x-10">
             <img
               className="w-64 rounded-full bg-white"
               src="https://1.bp.blogspot.com/-umW__JVzY78/Vf-aswLCMdI/AAAAAAAAyJg/hvvJJQqrxMI/s800/icon_business_man16.png"
+              alt="img"
             />
-            <ul className="pt-14 m-2 text-left ">
-              <li>Name:{guide?.name}</li>
-              <li>Country:{guide?.country}</li>
-              <li>City:{guide?.city}</li>
+            <ul className="pt-14 m-2 text-left leading-10">
+              <li className="font-semibold">Hello! I'm </li>
+              <li className="font-semibold text-center">{guide?.name}</li>
+              <li className="font-semibold">
+                I would like to guide for you in{' '}
+              </li>
+              <li className="font-semibold text-center">
+                {guide?.country} of {guide?.city}
+              </li>
             </ul>
           </div>
         </div>
-        <h2 className="text-center m-8">Bio: {guide?.bio}</h2>
+        <h2 className="  p-8 m-6 text-left rounded shadow-md bg-[#d2e6e3] leading-8">
+          {guide?.bio}
+        </h2>
+
         <div className="text-left flex justify-center">
-          <div className="border-2 white p-10 m-2">
-            <h2 className="text-center">About {guide?.name}</h2>
-            <h2>Spoken languages:</h2>
-            <ul>
-              <li>{guide?.language}</li>
-            </ul>
-            <h2>Fee: ${guide?.fee}</h2>
-            <h2>Contact Number: {guide?.contactNumber}</h2>
-            <h2>Email: {guide?.email}</h2>
+          <div className=" p-8 m-6 ounded shadow-md bg-[#d2e6e3]">
+            <h2 className="text-center">More about your local guide</h2>
+
+            <h2 className="m-2 leading-10">
+              Languages:
+              <ul>
+                <li>{guide?.language}</li>
+              </ul>
+            </h2>
+
+            <h2 className="m-2 leading-10">Fee: ${guide?.fee}</h2>
+            <h2 className="m-2 leading-10">
+              Contact Number: {guide?.contactNumber}
+            </h2>
+            <h2 className=" m-2 leading-10">Email: {guide?.email}</h2>
           </div>
         </div>
       </div>
