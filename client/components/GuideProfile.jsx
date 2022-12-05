386f61f86de2873a80b9cf14999c791d91ce48f8
Miyako Matsui <miyakomatsui18@gmail.com>
2022-12-05 15:15:46 +1300

    WIP add CSS

--- client/components/GuideProfile.jsx
+++ client/components/GuideProfile.jsx
@@ -24,47 +24,44 @@ function GuideProfile() {
   return (
     <div className="w-1/2 mx-auto text-center flex flex-col">
       <div className="font-[Lora] bg-[#C2DEDC] text-xl ">
-        <div className="border-2[#C2DEDC] block pt-16 break-all ">
-          <div className=" flex justify-center space-x-10">
+        <div className="block pt-16 break-nomal md:break-all ">
+          <div className=" flex justify-center  space-x-10">
             <img
-              className="w-64 rounded-full bg-white"
+              className="h-60 md:h-60 lg:h-60 rounded-full bg-white"
               src="https://1.bp.blogspot.com/-umW__JVzY78/Vf-aswLCMdI/AAAAAAAAyJg/hvvJJQqrxMI/s800/icon_business_man16.png"
               alt="img"
             />
-            <ul className="pt-14 m-2 text-left leading-10">
-              <li className="font-semibold">Hello! I'm </li>
+            <ul className="pt-10 m-3 p-6 text-left leading-10 break-nomal md:break-all">
+              <li className="font-normal">Hello! I'm </li>
               <li className="font-semibold text-center">{guide?.name}</li>
-              <li className="font-semibold">
-                I would like to guide for you in{' '}
-              </li>
+              <li className="font-normal">I would like to guide for you in </li>
               <li className="font-semibold text-center">
                 {guide?.country} of {guide?.city}
               </li>
             </ul>
           </div>
         </div>
-        <h2 className="  p-8 m-6 text-left rounded shadow-md bg-[#d2e6e3] leading-8">
-          {guide?.bio}
-        </h2>
 
-        <div className="text-left flex justify-center">
-          <div className=" p-8 m-6 ounded shadow-md bg-[#d2e6e3]">
-            <h2 className="text-center">More about your local guide</h2>
-
-            <h2 className="m-2 leading-10">
-              Languages:
-              <ul>
-                <li>{guide?.language}</li>
-              </ul>
-            </h2>
-
+        <h2 className="p-8 m-6 text-left rounded shadow-md bg-[#d2e6e3] leading-8">
+          <p className="text-center font-semibold">About {guide?.name}</p>
+          <p className="leading-10">{guide?.bio}</p>
+          <br />
+          <br />
+          <h2 className="text-center font-semibold">
+            More about your local guide
+          </h2>
+          <h2 className="m-2 leading-10">
+            Languages:
+            <ul>
+              <li>{guide?.language}</li>
+            </ul>
             <h2 className="m-2 leading-10">Fee: ${guide?.fee}</h2>
             <h2 className="m-2 leading-10">
               Contact Number: {guide?.contactNumber}
             </h2>
             <h2 className=" m-2 leading-10">Email: {guide?.email}</h2>
-          </div>
-        </div>
+          </h2>
+        </h2>
       </div>
       <ProfileDelete />
       <Review guideId={id} />
--- client/components/Header.jsx
+++ client/components/Header.jsx
@@ -5,17 +5,16 @@ import { Route, Routes } from 'react-router-dom'
 import Nav from './Nav'
 
 function Header() {
-
   return (
     <>
-    <div className='bg-[#DAB553] flex justify-between'>
-      <img className='h-40' src="./images/logo.png" alt="" />
-      <div className='flex flex-col'>
-        <h1 className='text-8xl m-1'>Zimoto</h1>
-        <h2 className='text-2xl ml-10'>Local, friendly guides</h2>
+      <div className="bg-[#DAB553] flex justify-between">
+        <img className="h-40" src="./images/logo.png" alt="" />
+        <div className="flex flex-col">
+          <h1 className="text-8xl m-1">Zimoto</h1>
+          <h2 className="text-2xl ml-10">Local, friendly guides</h2>
+        </div>
+        <Nav />
       </div>
-      <Nav />
-    </div>
     </>
   )
 }
