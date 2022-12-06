76612e9742d8b24d303f96cd8f9f2308c325a40d
Miyako Matsui <miyakomatsui18@gmail.com>
2022-12-06 15:32:24 +1300

    add style

--- client/components/Review.jsx
+++ client/components/Review.jsx
@@ -14,24 +14,26 @@ function Review({ guideId }) {
 
   return (
     <>
-      <div className="p-2">
-        <h1> Reviews: </h1>
+      <div className="p-2 leading-8">
+        <h1 className="font-semibold text-xl pt-8"> Reviews </h1>
         {reviews.length > 0 &&
           reviews.map((review) => {
             return (
               <div key={review.id}>
-                <div className="flex">
+                <div className="flex pt-8">
                   <p>
                     <img
                       src="../images/squareLogo.png"
                       alt="zimotologo"
-                      className="rounded-full h-10"
+                      className="rounded-full h-12"
                     />
                   </p>
-                  <p className="pt-2">Rating:{review.rating}</p>
+                  <p className="pt-2 pl-5 text-2xl">{review.rating} stars</p>
                 </div>
-                <p>Title:{review.title}</p>
-                <p>Text:{review.text}</p>
+                <p className="text-left text-xl p-5 leading-relaxed">
+                  {review.title}
+                </p>
+                <p className="text-left leading-6">{review.text}</p>
                 <ReviewEditDelete review={review} />
               </div>
             )
